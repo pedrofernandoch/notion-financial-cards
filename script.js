cbf53a2f28e6890187cd5fcd5c3f6470306d1e1c
Pedro Christofoletti <pedrofernandoch@gmail.com>
2022-04-05 23:29:53 -0300

    key test

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 $('#form').submit(function(e) {
-    $('#name').text($("#name-input").val());
-  
-    // Prevent reload page
+  $('#name').text($("#name-input").val());
     e.preventDefault();
   });
+
+$( document ).ready(function() {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+    });
+    $('#name').text(params.key);
+});
